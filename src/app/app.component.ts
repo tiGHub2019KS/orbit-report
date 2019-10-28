ff3151f7620945d795c1dd8db28fdcd60ee782fe
Kathy <kvsippo@charter.net>
2019-10-27 20:58:11 -0500

    this is orbit-report on 10/27/19 not finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
+let sourceList: Satellite[];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+  constructor() {
+    this.sourceList = [];
+
+
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then (function(response) {
+      response.json().then(function(data) {
+          let fetchedSatellites = data.satellites;
+          for(let i=0; i < fetchedSatellites.length; i++) {
+             let sat = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+             this.sourceList.push(sat);
+          }
+      }.bind(this));
+    }.bind(this));
+      
+  }
 }
